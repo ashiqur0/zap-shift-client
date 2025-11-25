4e6e4068e07d2a2880ffecab4efbe27b1295ae8b
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-11-25 14:03:14 +0600

    Layout: Update Root Layout

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -5,7 +5,7 @@ import Footer from '../UI/Footer';
 
 const RootLayout = () => {
     return (
-        <div>
+        <div className='md:max-w-7xl md:mx-auto'>
             <Navbar />
             <Outlet />
             <Footer />
