ddc913e320ebdd81440ff5d9ce92a12381b97680
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-11-25 09:29:08 +0600

    UI: Footer Component Added To RootLayout

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../UI/Navbar';
+import Footer from '../UI/Footer';
 
 const RootLayout = () => {
     return (
         <div>
             <Navbar />
             <Outlet />
+            <Footer />
         </div>
     );
 };
