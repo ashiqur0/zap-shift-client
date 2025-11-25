6f59d9a3ea5ec2715a9e8982c4eb7bf4b2bd02c8
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-11-25 09:17:00 +0600

    React Router: Basic Router Setup

---
+++ src/routes/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <h1>Hello world!111</h1>
+    }
+]);
+
+export default router;
