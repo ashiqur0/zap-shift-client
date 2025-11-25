dc49412230146735aef9bb094ebc8c23ac347d83
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-11-25 17:06:19 +0600

    Banner: Added Carousel Slide Images to Banner Component

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import bannerImage1 from '../../../assets/banner/banner1.png'
+import bannerImage2 from '../../../assets/banner/banner2.png'
+import bannerImage3 from '../../../assets/banner/banner3.png'
 
 const Banner = () => {
     return (
-        <div>
-            <h1>Banner</h1>
-        </div>
+        <Carousel>
+            <div>
+                <img src={bannerImage1} />
+                <p className="legend">Legend 1</p>
+            </div>
+            <div>
+                <img src={bannerImage2} />
+                <p className="legend">Legend 2</p>
+            </div>
+            <div>
+                <img src={bannerImage3} />
+                <p className="legend">Legend 3</p>
+            </div>
+        </Carousel>
     );
 };
 
