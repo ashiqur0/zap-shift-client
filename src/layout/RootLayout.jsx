057acd9fae322bef98d5afac9e9385ca368e9562
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-11-25 09:27:22 +0600

    UI: Navbar Added

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from '../UI/Navbar';
 
 const RootLayout = () => {
     return (
         <div>
+            <Navbar />
             <Outlet />
         </div>
     );
