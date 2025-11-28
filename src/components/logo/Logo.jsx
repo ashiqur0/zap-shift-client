fe31e105a3be7ee6b03a43ba835c86bb85c5c054
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-11-28 09:11:41 +0600

    Coverage Page: Implement Location Search Functionalities In leaflet Map

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from '../../assets/logo.png';
+import { Link } from 'react-router';
 
 const Logo = ({color='black'}) => {
     return (
-        <div className='flex items-end'>
+        <Link to={'/'} className='flex items-end'>
             <img className='w-8 h-auto' src={logo}></img>
             <h3 className={`text-2xl font-bold -ms-2 text-${color}`}>ZapShift</h3>
-        </div>
+        </Link>
     );
 };
 
--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useLoaderData } from 'react-router';
@@ -6,14 +6,50 @@ import { useLoaderData } from 'react-router';
 const Coverage = () => {
     const position = [23.6850, 90.3563];
     const service_centers = useLoaderData();
-    console.log(service_centers);
+    const mapRef = useRef(null);
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+
+        const location = e.target.location.value;
+        const district = service_centers.find(c => c.district.toLowerCase().includes(location.toLowerCase()));
+        if (district) {
+            const coord = [district.latitude, district.longitude];
+            // console.log(district, coord);
+            mapRef.current.flyTo(coord, 13);
+        }
+    }
 
     return (
         <div className='p-15 bg-white rounded-2xl my-15'>
-            <h1 className='md:text-3xl text-2xl font-extrabold text-secondary'>We are available in 64 districts</h1>
+            <h1 className='md:text-3xl text-2xl font-extrabold text-secondary mb-5'>We are available in 64 districts</h1>
+
+            <form onSubmit={handleSearch}>
+                <label className="input">
+                    <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <g
+                            strokeLinejoin="round"
+                            strokeLinecap="round"
+                            strokeWidth="2.5"
+                            fill="none"
+                            stroke="currentColor"
+                        >
+                            <circle cx="11" cy="11" r="8"></circle>
+                            <path d="m21 21-4.3-4.3"></path>
+                        </g>
+                    </svg>
+                    <input type="search" className="grow" placeholder="Search" name='location' />
+                </label>
+            </form>
 
             <div className='w-full h-[600px] mt-5'>
-                <MapContainer center={position} zoom={8} scrollWheelZoom={false} className='h-[600px]'>
+                <MapContainer
+                    center={position}
+                    zoom={8}
+                    scrollWheelZoom={false}
+                    className='h-[600px]'
+                    ref={mapRef}
+                >
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
