6cc17f3f795cd364ac4af018bf0f2793b3274ec5
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-11-28 23:19:35 +0600

    AuthProvider: Update AuthProvider

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,12 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AuthContext from '../context/AuthContext';
+import { createUserWithEmailAndPassword, GoogleAuthProvider } from 'firebase/auth/web-extension';
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
+import app from '../firebase/firebase.config';
+
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
-    
-    const authInfo = {
 
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUserWithEmail = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
     }
-    return <AuthContext value={authInfo}>{children}</AuthContext>
+
+    const loginWithEmail = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const loginWithGoogle = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+
+        return () => {
+            unsubscribe();
+        }
+    }, []);
+
+    const updateUser = (updatedData) => {
+        return updateProfile(auth.currentUser, updatedData);
+    }
+
+    const authInfo = {
+        createUserWithEmail,
+        loginWithEmail,
+        logOut,
+        loginWithGoogle,
+        updateUser,
+        user,
+        loading
+    }
+
+    return <AuthContext value={authInfo}> {children} </AuthContext>
 };
 
 export default AuthProvider;
