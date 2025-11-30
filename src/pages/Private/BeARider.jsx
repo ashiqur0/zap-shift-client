2987d7d0577b437c6bbc0f5d6abd3a363f86d9cb
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-11-30 16:44:28 +0600

    Update Be A Rider Page

--- src/pages/Private/BeARider.jsx
+++ src/pages/Private/BeARider.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const BeARider = () => {
     return (
-        <div>
+        <div className='md:max-w-7xl md:mx-auto p-15 bg-white my-20 rounded-2xl'>
             <h1>Be A Rider Page</h1>
         </div>
     );
