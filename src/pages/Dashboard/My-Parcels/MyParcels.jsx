66e21772a1fe9b14e8a1228bfd621002f1bd60c3
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-03 16:39:48 +0600

    MyParcel Page: Added Parcel Details

--- src/pages/Dashboard/My-Parcels/MyParcels.jsx
+++ src/pages/Dashboard/My-Parcels/MyParcels.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import useAuth from '../../../hooks/useAuth';
 import { useQuery } from '@tanstack/react-query';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import { FaEdit } from "react-icons/fa";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 const MyParcels = () => {
 
@@ -19,6 +22,37 @@ const MyParcels = () => {
     return (
         <div className='md:max-w-7xl md:mx-auto p-4'>
             <h1>All My Parcels({parcels.length})</h1>
+
+            <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>Si. No.</th>
+                            <th>Parcel Name</th>
+                            <th>Cost</th>
+                            <th>Payment Status</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            parcels.map((parcel, index) => <tr key={parcel._id}>
+                                <th>{index + 1}</th>
+                                <td>{parcel.parcelName}</td>
+                                <td>{parcel.cost}</td>
+                                <td></td>
+                                <td>
+                                    <button className='btn btn-square hover:bg-primary'><FaMagnifyingGlass />
+                                    </button>
+                                    <button className='btn btn-square hover:bg-primary'><FaEdit /></button>
+                                    <button className='btn btn-square hover:bg-primary'><FaRegTrashAlt /></button>
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/pages/Private/SendParcel/SendParcel.jsx
+++ src/pages/Private/SendParcel/SendParcel.jsx
@@ -51,6 +51,8 @@ const SendParcel = () => {
         }
 
         console.log('cost', cost, data);
+        data.cost = cost;
+
         Swal.fire({
             title: "Agree with the cost?",
             text: `You will be charged ${cost} taka`,
