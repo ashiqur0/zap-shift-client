3b43c2c34408f9107fc9ad633fbdddd2adf98238
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-03 15:17:26 +0600

    Use Tanstack Query wiht Axios to Fetch Data

--- src/pages/Dashboard/My-Parcels/MyParcels.jsx
+++ src/pages/Dashboard/My-Parcels/MyParcels.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import useAuth from '../../../hooks/useAuth';
+import { useQuery } from '@tanstack/react-query';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const MyParcels = () => {
+
+    const { user } = useAuth();
+    const axiosSecure = useAxiosSecure();
+
+    const { data: parcels = [] } = useQuery({
+        queryKey: ['myParcels', user?.email],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/parcels?email=${user.email}`)
+            return res.data;
+        }
+    })
+
     return (
-        <div>
-            <h1>My Parcels Page</h1>
+        <div className='md:max-w-7xl md:mx-auto p-4'>
+            <h1>All My Parcels({parcels.length})</h1>
         </div>
     );
 };
