5adcf40d7c989b017e20f3e415ab180a06149483
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-04 16:25:55 +0600

    Added Response Interceptors and Logout Mechanism If User Try Unauthorized or Forbidden Access of Data

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import { useEffect } from 'react';
 import useAuth from './useAuth';
+import { useNavigate } from 'react-router';
 
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000/'
@@ -9,17 +10,40 @@ const axiosSecure = axios.create({
 
 const useAxiosSecure = () => {
 
-    const { user } = useAuth();
+    const { user, logOut } = useAuth();
+    const navigate = useNavigate();
 
     useEffect(() => {
 
-        // intercept request
-        axiosSecure.interceptors.request.use((config) => {
+        // interceptor request
+        const reqInterceptor = axiosSecure.interceptors.request.use((config) => {
             config.headers.Authorization = `Berar ${user?.accessToken}`;
             return config;
         });
 
-    }, [user])
+        // interceptor response
+        const resInterceptor = axiosSecure.interceptors.response.use((response) => {
+            return response;
+        }, (error) => {
+            console.log(error);
+
+            const statuscode = error.status;
+            if (statuscode === 401 || statuscode === 403) {
+                logOut()
+                    .then(() => {
+                        navigate('/login');
+                    })
+            }
+
+            return Promise.reject(error);
+        })
+
+        return () => {
+            axiosSecure.interceptors.request.eject(reqInterceptor);
+            axiosSecure.interceptors.response.eject(resInterceptor);
+        }
+
+    }, [user, logOut, navigate])
 
     return axiosSecure;
 };
