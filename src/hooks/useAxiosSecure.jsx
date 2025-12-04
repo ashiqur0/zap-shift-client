e46fcfaa2208b158dd0b9d5f1f9bdd7d3a86b7bf
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-04 15:50:45 +0600

    Added Request Interceptor to Send and Verify Firebase Access Token

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import axios from 'axios';
+import { useEffect } from 'react';
+import useAuth from './useAuth';
 
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000/'
 })
 
 const useAxiosSecure = () => {
-    
+
+    const { user } = useAuth();
+
+    useEffect(() => {
+
+        // intercept request
+        axiosSecure.interceptors.request.use((config) => {
+            config.headers.Authorization = `Berar ${user?.accessToken}`;
+            return config;
+        });
+
+    }, [user])
+
     return axiosSecure;
 };
 
