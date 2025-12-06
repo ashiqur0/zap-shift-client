3144fd2c739f630ec0f66f20fdd96c8a30ca6e08
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-06 15:35:13 +0600

    Implement Handle Accept Delivery Functionalities When Rider Confirm The Delivery Taks

--- src/pages/Dashboard/Assign-Deliveries/AssignedDeliveries.jsx
+++ src/pages/Dashboard/Assign-Deliveries/AssignedDeliveries.jsx
@@ -2,19 +2,38 @@ import React from 'react';
 import useAuth from '../../../hooks/useAuth';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
+import Swal from 'sweetalert2';
 
 const AssignedDeliveries = () => {
 
     const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
 
-    const { data: parcels = [] } = useQuery({
+    const { data: parcels = [], refetch } = useQuery({
         queryKey: ['parcels', user.email, 'driver_assigned'],
         queryFn: async () => {
             const res = await axiosSecure.get(`/parcels/rider?riderEmail=${user.email}&deliveryStatus=driver_assigned`);
             return res.data;
         }
-    })
+    });
+
+    const handleAcceptDelivery = parcel => {
+        const statusInfo = { deliveryStatus: 'rider_arriving' };
+        axiosSecure.patch(`/parcels/${parcel._id}/status`, statusInfo)
+            .then(res => {
+                if (res.data.modifiedCount) {
+                    refetch();
+
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Thank you for accepting",
+                        showConfirmButton: false,
+                        timer: 2500
+                    });
+                }
+            })
+    }
 
     return (
         <div className='md:max-w-7xl md:mx-auto p-4'>
@@ -39,8 +58,16 @@ const AssignedDeliveries = () => {
                                 <td>{parcel.parcelName}</td>
                                 <td>{parcel.senderDistrict}</td>
                                 <td>
-                                    <button className='btn btn-sm btn-primary text-black'>Accept</button>
-                                    <button className='btn btn-sm btn-warning text-black ml-3'>Cancel</button>
+                                    <button
+                                        onClick={() => handleAcceptDelivery(parcel)}
+                                        className='btn btn-sm btn-primary text-black'
+                                    >Accept
+                                    </button>
+
+                                    <button
+                                        className='btn btn-sm btn-warning text-black ml-3'
+                                    >Cancel
+                                    </button>
                                 </td>
                             </tr>)
                         }
