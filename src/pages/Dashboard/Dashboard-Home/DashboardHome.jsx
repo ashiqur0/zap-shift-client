9965d4d1096802f5fa262d725783d81f81d7e52f
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-08 07:29:05 +0600

    Update Dashboard Home Page By Implementing Conditional Rendering Dashboard Home Page for Admin Rider and User

--- src/pages/Dashboard/Dashboard-Home/DashboardHome.jsx
+++ src/pages/Dashboard/Dashboard-Home/DashboardHome.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import useRole from '../../../hooks/useRole';
+import Loading from '../../../components/loading/Loading';
+import AdminDashboardHome from './AdminDashboardHome';
+import RiderDashboardHome from './RiderDashboardHome';
+import UserDashboardHome from './UserDashboardHome';
 
 const DashboardHome = () => {
-    return (
-        <div className='md:max-w-7xl md:mx-auto p-4'>
-            <h1>Dashboard Home Page</h1>
-        </div>
-    );
+
+    const { role, roleLoading } = useRole();
+
+    if (roleLoading) return <Loading />
+    if (role === 'admin') return <AdminDashboardHome />
+    else if (role === 'rider') return <RiderDashboardHome />
+
+    return <UserDashboardHome />
 };
 
 export default DashboardHome;
