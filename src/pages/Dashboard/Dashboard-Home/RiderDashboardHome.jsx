7e42ed9b708cc0ee4c28431b7309ed7a6b514d5f
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-08 06:59:15 +0600

    Create Personalized Dashboard Home Page For Rider

---
+++ src/pages/Dashboard/Dashboard-Home/RiderDashboardHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RiderDashboardHome = () => {
+    return (
+        <div className='md:max-w-7xl md:mx-auto p-4'>
+            <h1>Rider Dashboard Home Page</h1>
+        </div>
+    );
+};
+
+export default RiderDashboardHome;
