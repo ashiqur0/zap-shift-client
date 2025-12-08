aea27ecb760952ee09b67d432d0629e6eba306d7
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-08 07:00:44 +0600

    Create Personalized Dashboard Home Page For User

---
+++ src/pages/Dashboard/Dashboard-Home/UserDashboardHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserDashboardHome = () => {
+    return (
+        <div className='md:max-w-7xl md:mx-auto p-4'>
+            <h1>User Dashboard Home Page</h1>
+        </div>
+    );
+};
+
+export default UserDashboardHome;
